b26ddf1d19406338b5aa2ecf97430c2241f4e64a
etsh0 <ahmed.h333444@gmail.com>
2026-06-21 09:47:32 +0300

    feat: add tasks view select with icons and styling

--- src/features/tasks/components/TasksPageHeader.tsx
+++ src/features/tasks/components/TasksPageHeader.tsx
@@ -1,14 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import ArrowRight2 from '../../../assets/icons/ArrowRight2';
 import SearchInput from '../../../shared/components/SearchInput';
 import DashboardIcon from '../../../assets/icons/DashboardIcon';
-import ArrowDown from '../../../assets/icons/ArrowDown';
 import FilterIcon from '../../../assets/icons/FilterIcon';
 import { useBreadcrumb } from '../../../shared/hooks/useBreadcrumb';
 import type { BreadcrumbItem } from '../../../features/projects/type';
+import Select from 'react-select';
+import { type ReactNode } from 'react';
+import ListIcon from '../../../assets/icons/ListIcon';
+
+const tasksViewOptions: {
+  label: string;
+  value: 'board' | 'list';
+  icon: ReactNode;
+}[] = [
+  {
+    label: 'Board View',
+    icon: <DashboardIcon />,
+    value: 'board',
+  },
+  {
+    label: 'List View',
+    icon: <ListIcon className="w-[13.5px] h-[13.5px] text-primary" />,
+    value: 'list',
+  },
+];
 
 export default function TasksPageHeader() {
   const breadcrumb = useBreadcrumb();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const view = searchParams.get('view') ?? 'board';
+
+  const selectedOption = tasksViewOptions.find((o) => o.value === view);
+
   return (
     <>
       <header className="flex items-end justify-between mb-10">
@@ -48,12 +73,40 @@ export default function TasksPageHeader() {
         </div>
         <div className="flex items-center gap-3">
           <SearchInput placeholder="Search tasks..." />
-          <div className="py-2 px-4 rounded-sm border border-border bg-white flex items-center justify-center gap-2">
-            <DashboardIcon />
-            <span className="text-slate-one font-medium text-body-md leading-5">
-              Board View
-            </span>
-            <ArrowDown />
+          <div className="">
+            <Select
+              value={selectedOption}
+              options={tasksViewOptions}
+              onChange={(option) => {
+                setSearchParams({ view: option!.value });
+              }}
+              isClearable={false}
+              isSearchable={false}
+              components={{
+                IndicatorSeparator: () => null,
+              }}
+              formatOptionLabel={(option) => (
+                <div className="flex items-center gap-2">
+                  <span className="text-slate-one">{option.icon}</span>
+                  <span className="text-slate-one font-medium text-body-md leading-5">
+                    {option.label}
+                  </span>
+                </div>
+              )}
+              styles={{
+                control: (base) => ({
+                  ...base,
+                  borderRadius: '4px',
+                  border: 'none',
+                  boxShadow: '0px 1px 2px 0px #0000000D',
+                  fontSize: '14px',
+                  fontWeight: '500',
+                  color: '#041B3C',
+                  lineHeight: '20px',
+                  cursor: 'pointer',
+                }),
+              }}
+            />
           </div>
           <div className="p-2 rounded-sm bg-surface-highest">
             <FilterIcon />
--- src/pages/projects/TasksPage.tsx
+++ src/pages/projects/TasksPage.tsx
@@ -1,22 +1,23 @@
 import { useSearchParams } from 'react-router-dom';
 import TasksPageHeader from '../../features/tasks/components/TasksPageHeader';
 import BoardView from '../../features/tasks/components/kanban/BoardView';
+import ListView from '../../features/tasks/components/list/ListView';
 
 export default function TasksPage() {
   const [searchParams] = useSearchParams();
   const view = searchParams.get('view') ?? 'board';
   return (
     <>
-      <div className="">
+      <div className="py-8 px-6 lg:px-8">
+        <TasksPageHeader />
         {view === 'board' ? (
-          <section className="py-8 px-6 lg:px-8">
-            <TasksPageHeader />
-            <div className="board-view mt-6">
-              <BoardView />
-            </div>
+          <section className="board-view mt-6">
+            <BoardView />
           </section>
         ) : (
-          <section>List</section>
+          <section className="list-view mt-6">
+            <ListView />
+          </section>
         )}
       </div>
     </>
