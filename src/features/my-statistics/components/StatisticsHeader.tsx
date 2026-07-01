c3bafc67cb5238745915f82d1a2d18f98211de98
etsh0 <ahmed.h333444@gmail.com>
2026-07-01 17:07:53 +0300

    feat: (My-Statistics) add daypicker and handle ui

--- src/features/my-statistics/components/StatisticsHeader.tsx
+++ src/features/my-statistics/components/StatisticsHeader.tsx
@@ -1,8 +1,12 @@
 import Select from 'react-select';
 import ArrowLeft2 from '../../../assets/icons/ArrowLeft2';
 import ArrowRight2 from '../../../assets/icons/ArrowRight2';
+import { useState } from 'react';
+import { DayPicker } from '@daypicker/react';
+import Button from '../../../shared/components/Button';
 
 export default function StatisticsHeader() {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <header className="flex flex-col mb-10 gap-8">
@@ -15,12 +19,45 @@ export default function StatisticsHeader() {
           </p>
         </div>
         <div className="p-4 bg-surface-low rounded-lg flex items-center justify-between w-full">
-          <div className="flex items-center gap-3">
-            <ArrowLeft2 />
-            <div className="text-label-sm text-slate-one font-bold leading-5">
-              May 11 - May 17, 2025
+          <div className="relative">
+            <div
+              onClick={() => setOpen(!open)}
+              className="flex items-center gap-3 cursor-pointer"
+            >
+              <ArrowLeft2 />
+              <div className="text-label-sm text-slate-one font-bold leading-5">
+                May 11 - May 17, 2025
+              </div>
+              <ArrowRight2 />
             </div>
-            <ArrowRight2 />
+            {open && (
+              <div className="absolute -left-4 top-8 bg-white/70 rounded-lg p-5 border border-border backdrop-blur-[20px]">
+                <DayPicker
+                  mode="range"
+                  showOutsideDays
+                  classNames={{
+                    day: 'text-[12px] font-semibold',
+                    selected:
+                      '[&_button]:rounded-lg [&_button]:bg-primary/20 [&_button]:text-primary',
+                    range_start: 'bg-[#0052CC33] text-primary ',
+                    range_middle: 'bg-[#0052CC33] text-primary ',
+                    range_end: 'bg-[#0052CC33] text-primary ',
+                  }}
+                />
+                <div className="pt-4 border-t border-border flex items-center justify-between">
+                  <button
+                    type="button"
+                    onClick={() => setOpen(!open)}
+                    className="text-[12px] text-neutral w-full font-medium leading-4 rounded-lg md:rounded-xs py-2.5 px-6  cursor-pointer"
+                  >
+                    Cancel
+                  </button>
+                  <div className="w-full">
+                    <Button>Apply Range</Button>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
           <div className="flex items-center gap-4">
             <div className="">
